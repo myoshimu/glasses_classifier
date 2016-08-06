94eef23bf54abe02ab82eb33be9477158dd563af
myoshimu <miki.katsuragi1@gmail.com>
2016-08-07 00:56:07 +0900

    チューニング精度変更

--- SVM_optimize.R
+++ SVM_optimize.R
@@ -1,30 +1,21 @@
 library(e1071)
 library(biOps)
 
-# 作業ディレクトリの指定
 kBaseDir <-"./" 
 
-# 縮小サイズ
 kXBase = 48
 kYBase = 27
 kXYVecMax = kXBase * kYBase
 
 # グリッドサーチ用変数
-# 大雑把な探索
 kRoughGammaRange <-c(2^(seq(-15,3,3)))
 kRoughCostRange <-c(2^(seq(-5,15,3)))
-# 細かな探索
-kDetailGammaRange <-c(2^(seq(-16,-14,0.5)))
-kDetailCostRange <-c(2^(seq(5,7,0.5)))
 
 CreateDataset <- function(){
-  # 画像オブジェクト生成
   gl_imgs <- ReadImages("./pika/")
   none_imgs <- ReadImages("./no_pika/")
-  # 素性ベクトル作成
   gl_df <- ConvertImagesToFeatureVectors(gl_imgs, "pika")
   none_df <- ConvertImagesToFeatureVectors(none_imgs, "none")
-  # データセット作成
   df <- rbind(gl_df, none_df)
   return(df)  
 }
@@ -48,7 +39,6 @@ ReadImages <- function(dir){
   return(images)
 }
 
-# 読み込んだ jpg を kXBase x kYBase にダウンサイジング
 DoDownsising <- function(img){
   x_size <- ncol(img)
   y_size <- nrow(img)
@@ -59,42 +49,24 @@ DoDownsising <- function(img){
 }
 
 
-# 読み込んだ jpg を グレースケール変換
 ToGrayscale <- function(img){
   return(imgRGB2Grey(img, coefs=c(0.30, 0.59, 0.11)))
 }
 
-# 読み込んだ jpg を エッジ強調
 EmphasizeEdge <- function(img){
   img <- imgCanny(img, sigma=0.4)
   return(img)
 }
 
-# 素性ベクトル作成
 ConvertImageToFeatureVector <- function(img){
-  # 1. 元画像
-  # plot(img)
-  # browser()
-  # 2. グレースケール変換
-  # black: 0, white: 255
   g_img <- ToGrayscale(img)
-  # plot(g_img)
-  # browser()  
-  # 3. エッジ強調
   ge_img <- EmphasizeEdge(g_img)
-  # plot(ge_img)
-  # browser()  
-  # 4. ダウンサイジング
   ged_img <- DoDownsising(ge_img)
-  # plot(ged_img)
-  # browser()  
   vec <- as.vector(ged_img)
-  # black: 0 to white: 1 
   normalized_vec <- vec / 255
   return(normalized_vec)
 }
 
-# 全イメージを素性ベクトルに変換
 ConvertImagesToFeatureVectors <- function(imgs, label){
   vectors_list <- lapply(imgs, ConvertImageToFeatureVector)
   vectors_list <- lapply(vectors_list, (function(x) {x[1:kXYVecMax]}))
@@ -135,12 +107,6 @@ TuneSVM(dataset, kRoughGammaRange, kRoughCostRange)
 # gamma = 3.051758e-05 ; cost = 128 ;
 # accuracy: 70.33333 %
 
-# 細かなグリッドサーチ
-TuneSVM(dataset, kDetailGammaRange, kDetailCostRange)
-# - best parameters:
-# gamma = 2.157919e-05 ; cost = 64 ;
-# accuracy: 70.4698 %
-
 # モデルの生成
 model <- svm(
   label ~ .,
@@ -149,7 +115,7 @@ model <- svm(
   cost = 16
   )
 
-# 新しいデータを分類してみる
+# 新しいデータを分類
 gl_samples_df <- CreateSamples("pika")
 result <- predict(model, gl_samples_df)
 print(result)
@@ -163,3 +129,4 @@ print(result)
 # 1       2       3 
 # none    none pika 
 # Levels: pika none
+Contact GitHub API Training Shop Blog About
